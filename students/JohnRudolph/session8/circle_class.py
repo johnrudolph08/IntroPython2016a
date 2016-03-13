ec63f4aad6efb82bb315bc0db150c2360322fb16
John Rudolph <john.rudolph08@gmail.com>
2016-03-12 22:36:56 -0800

    completed circle lab

--- students/JohnRudolph/session8/circle_class.py
+++ students/JohnRudolph/session8/circle_class.py
@@ -6,22 +6,22 @@ from math import pi
 ######################################################
 
 class Circle(object):
+	'''
+	Defines a circle object which takes
+	A radius or diameter as an input
+	And performs some operations
+	'''
 
-	def __init__(self, radius):
-		self._radius = radius
-		self._diameter = (radius*2)
-		self._area = (pi*radius**2)
-
-	def __str__(self):
-		return 'Circle with radius: {}'.format(self._radius)
-
-	#cant get this to work
-	def __repr__(self):
-		return 'Circle(%s)' % super (Circle, self).__repr__()
-
-	@classmethod
-	def from_diameter(cls, diameter):
-		return cls(diameter/2)
+	#init takes radius or diameter as an input
+	def __init__(self, **kwargs):
+		if [x for x in kwargs if x not in ['radius', 'diameter']]:
+			raise AttributeError('radius or diameter is required')
+		#if radius then set radius
+		if 'radius' in kwargs:
+			self._radius = kwargs['radius']
+		#if diameter then set radius
+		if'diameter' in kwargs:
+			self._radius = kwargs['diameter']/2
 
 	@property
 	def radius(self):
@@ -29,20 +29,38 @@ class Circle(object):
 
 	@property
 	def diameter(self):
-		return self._diameter
-	
+		return self.radius*2
+
 	@property
 	def area(self):
-		return self._area
+		return self._radius**2*pi
 
 	@radius.setter
 	def radius(self, radius):
 		self._radius = radius
-		self._diameter = (radius*2)
 
 	@diameter.setter
 	def diameter(self, diameter):
-		self._diameter = diameter
-		self._radius = (diameter/2)
+		self._radius = diameter/2
 
+	def __repr__(self):
+		return 'Circle({})'.format(self.radius)
 
+	def __str__(self):
+		return 'Circle with radius: {}'.format(self.radius)
+	#set __add__ method to create new circle class with added radius values
+	def __add__(self, other):
+		add_radius= self.radius + other.radius
+		return Circle(radius= add_radius)
+	#set __mul__ method to create new circle class with multiplied radius
+	def __mul__(self, num):
+		return Circle(radius=(self.radius*num))
+	#set comparison operators
+	def __gt__(self, other):
+		return self.radius > other.radius
+
+	def __lt__(self, other):
+		return self.radius < other.radius
+
+	def __eq__(self, other):
+		return self.radius == other.radius
--- students/JohnRudolph/session8/test_circle_class.py
+++ students/JohnRudolph/session8/test_circle_class.py
@@ -8,7 +8,7 @@ def test_radius():
 	and called via get property
 	'''
 	radius = 4
-	c = cc.Circle(4)
+	c = cc.Circle(radius=radius)
 	assert radius == c.radius
 
 def test_diameter():
@@ -16,7 +16,7 @@ def test_diameter():
 	test that diameter is set when radius is provided
 	'''
 	radius = 4
-	c = cc.Circle(4)
+	c = cc.Circle(radius=radius)
 	assert radius*2 == c.diameter
 
 def test_set_diameter():
@@ -24,7 +24,8 @@ def test_set_diameter():
 	test that a new diameter can be set based on set property
 	test that radius is reset when diameter is set
 	'''
-	c = cc.Circle(4)
+	radius = 4
+	c = cc.Circle(radius=radius)
 	new_diameter = 2
 	c.diameter = new_diameter
 	assert c.diameter == new_diameter
@@ -34,7 +35,8 @@ def test_area():
 	'''
 	test that an area attribute is set when Circle is init
 	'''
-	c = cc.Circle(4)
+	radius = 4
+	c = cc.Circle(radius=radius)
 	area = (c.radius**2*pi)
 	assert c.area == area
 
@@ -42,7 +44,8 @@ def test_area_error():
 	'''
 	Check if setting area raises an Attribute Error
 	'''
-	c = cc.Circle(4)
+	radius = 4
+	c = cc.Circle(radius=radius)
 	with pytest.raises(AttributeError):
 		c.area = 2
 
@@ -51,7 +54,7 @@ def test_alternate_constructor():
 	Check if setting area raises an Attribute Error
 	'''
 	diameter = 8
-	c = cc.Circle.from_diameter(diameter)
+	c = cc.Circle(diameter = diameter)
 	assert c.radius == (diameter/2)
 	
 def test_str_method():
@@ -59,7 +62,7 @@ def test_str_method():
 	Check if __str__ method works as intended
 	'''
 	radius = 4
-	c = cc.Circle(radius)
+	c = cc.Circle(radius = radius)
 	s = 'Circle with radius: {}'.format(radius)
 	assert s == str(c)
 
@@ -68,13 +71,69 @@ def test_repr_method():
 	Check if __str__ method works as intended
 	'''
 	radius = 4
-	c = cc.Circle(radius)
+	c = cc.Circle(radius=radius)
 	s = 'Circle({})'.format(radius)
-	repr(c)
-	d = eval(repr(c))
+	d = repr(c)
 	assert s == d
 
-c = Circle(4)
-repr(c)
-d = eval(repr(c))
-print(d)
+def test_add_method():
+	'''
+	Check if 2 circles can be added	
+	'''
+	radius = 4
+	diameter = 10
+	c1 = cc.Circle(radius = radius)
+	c2 = cc.Circle(diameter= diameter)
+	c3 = c1 + c2
+	assert c3.radius == radius + diameter/2
+
+def test_mult_method():
+	'''
+	Check if 2 circles can be added	
+	'''
+	radius = 4
+	num = 3
+	c1 = cc.Circle(radius = radius)
+	c2 = c1*num
+	assert c2.radius == radius*num
+
+def test_gt_method():
+	'''
+	Check if circle1 radius greater than circle2 radius
+	'''
+	radius1 = 4
+	radius2 = 6
+	c1 = cc.Circle(radius=radius1)
+	c2 = cc.Circle(radius=radius2)
+	assert (c2 > c1) == True
+
+def test_lt_method():
+	'''
+	Check if circle1 radius less than circle2 radius
+	'''
+	radius1 = 4
+	radius2 = 6
+	c1 = cc.Circle(radius=radius1)
+	c2 = cc.Circle(radius=radius2)
+	assert (c1 < c2) == True
+
+def test_eq_method():
+	'''
+	Check if circle1 radius less than circle2 radius
+	'''
+	radius1 = 6
+	radius2 = 6
+	c1 = cc.Circle(radius=radius1)
+	c2 = cc.Circle(radius=radius2)
+	assert (c1 == c2) == True
+
+def test_sort_method():
+	'''
+	Test of a list of circle objects is sortable
+	'''
+	c1, c2, c3 = cc.Circle(radius=4), cc.Circle(radius=7), cc.Circle(radius=2)
+	circles = [c1, c2, c3]
+	sort_circles = [c3, c1, c2]
+	for i,n in zip(sorted(circles), sort_circles):
+		assert i.radius == n.radius
+	
